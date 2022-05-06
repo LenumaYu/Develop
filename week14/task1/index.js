ad04d4d17aa92d74157bb25627010df0773bb234
LenumaYu <elena_guseva_92@mail.ru>
2022-05-06 17:15:13 +0700

    Внесла правки

--- week14/task1/index.js
+++ week14/task1/index.js
@@ -1,27 +1,6 @@
-function getSum() {
-    let a = Number(document.getElementById('firstNumber').value);
-    let b = Number(document.getElementById('secondNumber').value);
-    let result = a + b;
-    document.getElementById('result').innerHTML = result;
-}
+function getMessage() {
 
-function getSubstraction() {
-    let a = Number(document.getElementById('firstNumber').value);
-    let b = Number(document.getElementById('secondNumber').value);
-    let result = a - b;
-    document.getElementById('result').innerHTML = result;
-}
-
-function getDivision() {
-    let a = Number(document.getElementById('firstNumber').value);
-    let b = Number(document.getElementById('secondNumber').value);
-    let result = a / b;
-    document.getElementById('result').innerHTML = result;
-}
-
-function getMultiplication() {
-    let a = Number(document.getElementById('firstNumber').value);
-    let b = Number(document.getElementById('secondNumber').value);
-    let result = a * b;
-    document.getElementById('result').innerHTML = result;
+    let userName = document.getElementById('name').value;
+    let greeting = "Привет," + userName + "!";
+    document.getElementById('greeting').innerHTML = greeting;
 }

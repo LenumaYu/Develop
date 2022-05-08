845d78e35f4715c6004c9fbcd76b10cd2760ff4c
LenumaYu <elena_guseva_92@mail.ru>
2022-05-08 18:48:47 +0700

    Task 3 finished

---
+++ week14/task3/index.js
@@ -0,0 +1,9 @@
+function changePicture() {
+    let firstPicture = document.getElementById("firstPicture");
+    firstPicture.src = "assets/images/picture2.jpg";
+}
+
+function changeBackPicture() {
+    let firstPicture = document.getElementById("firstPicture");
+    firstPicture.src = "assets/images/picture1.jpg";
+}

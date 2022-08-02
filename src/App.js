a634790049d94a3bdce46f0f7c7410a5c447dc39
LenumaYu <elena_guseva_92@mail.ru>
2022-08-02 20:12:48 +0700

    task for week 25 completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Chart } from "react-google-charts";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+      <Chart
+  chartType="PieChart"
+  data={[
+    ["Pet", "%"],
+    ["Собаки", 39],
+    ["Кошки", 46],
+    ["Кролики", 7],
+    ["Рыбки", 6],
+    ["Другие питомцы", 2],
+  ]}
+  width={'700px'}
+  height={'700px'}
+  options={{
+    title: "Каких домашних питомцев заводят в России",
+    slices: {
+      0: { color: "#d6cbd3" },
+      1: { color: "#eca1a6" },
+      2: { color: "#bdcebe" },
+      3: { color: "#ada397" },
+      4: { color: "#92a8d1" },
+    },
+  }}
+/>
   );
 }
 

36c05e97a814e435ce0b8935f8cac9f04429ecfa
LenumaYu <elena_guseva_92@mail.ru>
2022-08-20 19:53:36 +0700

    first task finished

---
+++ week27/tariff/src/components/TariffCard.jsx
@@ -0,0 +1,15 @@
+import './styles.scss'
+
+
+function TariffCard(props) {
+    return (
+        <div className={"card " + (props.isSelected ? "selected" : "")}>
+            <div className='head' style={{ backgroundColor: props.backgroundColorHead }}>{props.name}</div>
+            <div className='price' style={{ backgroundColor: props.backgroundColorPrice }}>{props.price}</div>
+            <div className='speed'>{props.speed}</div>
+            <div className='info'>{props.info}</div>
+        </div >
+    );
+}
+
+export default TariffCard;

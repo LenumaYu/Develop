1a4bce7d5fb38db3e6ce3ad546a7645b869fb1ea
LenumaYu <elena_guseva_92@mail.ru>
2022-08-22 13:54:08 +0700

    карточки и стрелочки стилизованы

--- Course project/dictionary/src/components/WordCard.jsx
+++ Course project/dictionary/src/components/WordCard.jsx
@@ -4,11 +4,15 @@ import './WordCard.scss'
 
 function WordCard(props) {
     return (
-        <div className="card">
-            <div>{props.tags}</div>
-            <div>{props.english}</div>
-            <div>{props.transcription}</div>
-            <div>{props.russian}</div>
+        <div className='container'>
+            <button className='leftArrow'>&#706;</button>
+            <div className="card">
+                <div className='tag'>{props.tags}</div>
+                <div className='word'>{props.english}</div>
+                <div className='transcription'>{props.transcription}</div>
+                <div className='translate'>{props.russian}</div>
+            </div>
+            <button className='rightArrow'>&#707;</button>
         </div>
     );
 }

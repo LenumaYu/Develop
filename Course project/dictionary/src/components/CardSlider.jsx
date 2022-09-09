c7b8d177b6dcff1ea0d007f2b9ab102a1a5a2a50
LenumaYu <elena_guseva_92@mail.ru>
2022-09-09 13:49:23 +0700

    добавила useEffect

--- Course project/dictionary/src/components/CardSlider.jsx
+++ Course project/dictionary/src/components/CardSlider.jsx
@@ -1,22 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import WordCard from './WordCard/WordCard';
+import './CardSlider.scss'
 
 function CardSlider(props) {
-    const [count, setCount] = useState(0);
-    const { id, ...itemProps } = props.words[count];
+    const [item, setItem] = useState(0);
+    const { id, ...itemProps } = props.words[item];
+
+
+    useEffect(() => {
+        const lastItem = props.words.length - 1;
+        if (item < 0) {
+            setItem(lastItem)
+        }
+        if (item > lastItem) {
+            setItem(0);
+        }
+    }, [item, props.words.length]);
+
 
     return (
-        <div className='container'>
+        <>
 
-            <button className='leftArrow' onClick={() => setCount(count - 1)}>&#706;</button>
-
-            <WordCard key={id} {...itemProps} />
-            <button className='rightArrow' onClick={() => setCount(count + 1)}>&#707;</button>
-            <div>{count + 1}/{props.words.length}</div>
-        </div>
-
+            <div className='slider'>
+                <button className='leftArrow' onClick={() => setItem(item - 1)}>&#706;</button>
+                <WordCard key={id} {...itemProps} />
+                <button className='rightArrow' onClick={() => setItem(item + 1)}>&#707;</button>
+            </div>
+            <div className='counter'>{item + 1}/{props.words.length}</div>
+        </>
 
     );
 }
 
 export default CardSlider;
+
+

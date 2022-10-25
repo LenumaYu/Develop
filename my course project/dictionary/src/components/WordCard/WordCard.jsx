4d5bc3d0aeb72ac449d2eb61aeecb447dacac638
LenumaYu <elena_guseva_92@mail.ru>
2022-10-25 23:01:29 +0700

    task 2

---
+++ my course project/dictionary/src/components/WordCard/WordCard.jsx
@@ -0,0 +1,20 @@
+import './WordCard.scss'
+
+
+function WordCard(props) {
+
+    const { tags, english, transcription, russian } = props;
+
+    return (
+        <div className='container'>
+            <div className="card">
+                <div className='tag'>{tags}</div>
+                <div className='word'>{english}</div>
+                <div className='transcription'>{transcription}</div>
+                <div className='translate'>{russian}</div>
+            </div>
+        </div>
+    );
+}
+
+export default WordCard;
